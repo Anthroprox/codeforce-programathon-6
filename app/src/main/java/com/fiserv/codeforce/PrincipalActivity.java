9f6129a993d3a3a0acafe0f98d1cfc95e1c9c5ba
Rody <rodycamposgo@gmail.com>
2019-10-26 13:49:33 -0600

    Add menu working

--- app/src/main/java/com/fiserv/codeforce/PrincipalActivity.java
+++ app/src/main/java/com/fiserv/codeforce/PrincipalActivity.java
@@ -1,14 +1,24 @@
 package com.fiserv.codeforce;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.MenuItem;
+
+
+import com.google.android.material.navigation.NavigationView;
 
 import org.androidannotations.annotations.EActivity;
 
 @EActivity(R.layout.activity_principal)
-public class PrincipalActivity extends Activity {
+public class PrincipalActivity extends Activity implements NavigationView.OnNavigationItemSelectedListener {
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        return false;
+    }
 
 //    @Override
 //    protected void onCreate(Bundle savedInstanceState) {
