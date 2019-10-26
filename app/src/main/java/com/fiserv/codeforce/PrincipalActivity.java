352d9222db3fe18905d5207f547995a17d71ee18
Rody <rodycamposgo@gmail.com>
2019-10-26 11:31:13 -0600

    Principal Activity

---
+++ app/src/main/java/com/fiserv/codeforce/PrincipalActivity.java
@@ -0,0 +1,18 @@
+package com.fiserv.codeforce;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import org.androidannotations.annotations.EActivity;
+
+@EActivity(R.layout.activity_principal)
+public class PrincipalActivity extends Activity {
+
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_principal);
+//    }
+}
