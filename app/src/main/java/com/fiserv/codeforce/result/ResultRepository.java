877c55a753d5aaf3a40d388c94775c53273dc13a
Bryan RD <bryanr9418@hotmail.com>
2019-10-27 02:53:27 -0600

    Add Interceptor

--- app/src/main/java/com/fiserv/codeforce/result/ResultRepository.java
+++ app/src/main/java/com/fiserv/codeforce/result/ResultRepository.java
@@ -6,6 +6,7 @@ import androidx.annotation.RequiresPermission;
 
 
 import com.fiserv.codeforce.login.ResponseLogin;
+import com.fiserv.codeforce.student.AuthorizationRepositoryInterceptor;
 
 import org.androidannotations.rest.spring.annotations.Body;
 import org.androidannotations.rest.spring.annotations.Post;
@@ -13,7 +14,7 @@ import org.androidannotations.rest.spring.annotations.Rest;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 
-@Rest(rootUrl = "http://192.168.128.22:25281/ApiServer/api/", converters = {MappingJackson2HttpMessageConverter.class})
+@Rest(rootUrl = "http://192.168.128.22:25281/ApiServer/api/", converters = {MappingJackson2HttpMessageConverter.class}, interceptors = AuthorizationRepositoryInterceptor.class)
 public interface ResultRepository {
 
     @RequiresPermission(Manifest.permission.INTERNET)
